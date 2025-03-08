f8e2599b72b6d1280333c0a9ed0c387c1b8800c8
Omid Shojaeian Zanjani <46863450+omidshz100@users.noreply.github.com>
2025-03-08 10:30:22 +0330

    Initial Commit

---
+++ CleanSleep/CleanSleepApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanSleepApp.swift
+//  CleanSleep
+//
+//  Created by Omid Shojaeian Zanjani on 08/03/25.
+//
+
+import SwiftUI
+
+@main
+struct CleanSleepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
